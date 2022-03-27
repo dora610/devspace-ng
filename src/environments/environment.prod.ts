1cefabc29990c039042d2088a5231b2a914a4f1e
Surajit Karuri <surajitkaruri@gmail.com>
2022-03-27 16:35:23 +0530

    added env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD6b7CSnmAkqPp8thcku2bSrx3h34k99tc',
+    authDomain: 'angular-project-94fea.firebaseapp.com',
+    projectId: 'angular-project-94fea',
+    storageBucket: 'angular-project-94fea.appspot.com',
+    messagingSenderId: '1097032948336',
+    appId: '1:1097032948336:web:5cbe39850d39ef46e1f2b5',
+  },
 };
